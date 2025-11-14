18407a473eb248781ba951107dbf39ce21f8c2ad
Shena <shenaco18@gmail.com>
2025-11-14 03:25:19 -0500

    Display plants with mapand Add To Cart functionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useDispatch } from 'react-redux';
+import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({})
+    const dispatch = useDispatch();
+    const handleAddToCart = (plant)=> {
+        dispatch(addItem(plant));
+        setAddedToCart((prev) => ({
+            ...prev,
+            [plant.name]: true
+        }));
+    }
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -274,6 +284,30 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
+                    {plantsArray.map((categoryObj)=> (
+                        <div key={categoryObj.category}>
+                            <h2>{categoryObj.category}</h2>
+                            <div className="plants">
+                                {categoryObj.plants.map((plant)=>(
+                                    <div key={plant.name} className="plant-card">
+                                        <h3>{plant.name}</h3>
+                                        <img src={plant.image} alt={plant.name}/>
+                                        <p>{plant.description}</p>
+                                        <p>{plant.cost}</p>
+                                        <button
+                                            onClick={()=> handleAddToCart(plant)}
+                                            disabled={addedToCart[plant.name]}
+                                            >
+                                            {addedToCart[plant.name]? "Added to Cart": "Add to Cart"}
+                                        </button>
+
+                                    </div>
+
+                                ))}
+
+                            </div>
+                        </div>
+                    ))}
 
 
                 </div>
